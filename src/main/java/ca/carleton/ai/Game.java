e6b3b0653da1c6db2d584837bb8b5b23c49f45b9
Michael Hum <michaelhum@cmail.carleton.ca>
2016-03-05 14:52:15 -0500

    Initial commit

---
+++ src/main/java/ca/carleton/ai/Game.java
@@ -0,0 +1,14 @@
+package ca.carleton.ai;
+
+/**
+ * Main class.
+ * <p>
+ * Created by Mike on 3/5/2016.
+ */
+public class Game {
+
+    public static void main(final String[] args) {
+
+    }
+
+}
